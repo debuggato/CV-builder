aaa242af261bd5a6781caed9be0aa7302b52de76
Ivan Lori <ivan.lori@protonmail.com>
2020-04-17 11:33:56 +0200

    adds alias server side and improves logic

--- src/server/index.js
+++ src/server/index.js
@@ -1,76 +1,100 @@
+import 'module-alias/register';
 import puppeteer from 'puppeteer';
 import express from 'express';
 import bodyParser from 'body-parser';
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import { renderToStaticMarkup } from 'react-dom/server';
 import cors from 'cors';
 
-import { SERVER_PORT } from '../utils/endpoints';
-import DaVinci from './templates/davinci/DaVinci.view';
-import Caravaggio from './templates/caravaggio/Caravaggio.view';
-import Donatello from './templates/donatello/Donatello.view';
-import Michelangelo from './templates/michelangelo/Michelangelo.view';
-import Raffaello from './templates/raffaello/Raffaello.view';
+import { SERVER_PORT, CLIENT_URL } from '@utils/endpoints';
+import DaVinci from '@server/templates/davinci/DaVinci.view';
+import Caravaggio from '@server/templates/caravaggio/Caravaggio.view';
+import Donatello from '@server/templates/donatello/Donatello.view';
+import Michelangelo from '@server/templates/michelangelo/Michelangelo.view';
+import Raffaello from '@server/templates/raffaello/Raffaello.view';
 
 const app = express();
 const sheet = new ServerStyleSheet();
 
+const corsOptions = {
+  origin: CLIENT_URL,
+  optionsSuccessStatus: 200,
+  methods: 'POST',
+};
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
-
-function getTemplate(selected, props) {
-  switch (selected) {
-    case '0':
-      return <DaVinci {...props} />;
-    case '1':
-      return <Michelangelo {...props} />;
-    case '2':
-      return <Donatello {...props} />;
-    case '3':
-      return <Raffaello {...props} />;
-    case '4':
-      return <Caravaggio {...props} />;
-  }
-}
+app.use(cors(corsOptions));
 
 app.post('/render', async (req, res, next) => {
-  const data = req.body;
+  try {
+    const data = req.body;
 
-  const component = renderToString(
-    <StyleSheetManager sheet={sheet.instance}>
-      {getTemplate(data.templateSelected, data)}
-    </StyleSheetManager>,
-  );
+    let component;
+    if (data.selected === '0') {
+      component = renderToStaticMarkup(
+        <StyleSheetManager sheet={sheet.instance}>
+          <DaVinci {...data} />
+        </StyleSheetManager>,
+      );
+    } else if (data.selected === '1') {
+      component = renderToStaticMarkup(
+        <StyleSheetManager sheet={sheet.instance}>
+          <Michelangelo {...data} />
+        </StyleSheetManager>,
+      );
+    } else if (data.selected === '2') {
+      component = renderToStaticMarkup(
+        <StyleSheetManager sheet={sheet.instance}>
+          <Donatello {...data} />
+        </StyleSheetManager>,
+      );
+    } else if (data.selected === '3') {
+      component = renderToStaticMarkup(
+        <StyleSheetManager sheet={sheet.instance}>
+          <Raffaello {...data} />
+        </StyleSheetManager>,
+      );
+    } else {
+      component = renderToStaticMarkup(
+        <StyleSheetManager sheet={sheet.instance}>
+          <Caravaggio {...data} />
+        </StyleSheetManager>,
+      );
+    }
 
-  const css = sheet.getStyleTags();
+    const css = sheet.getStyleTags();
 
-  const html = `
+    const html = `
     <html>
       <head>${css}</head>
       <body>${component}</body>
     </html>
   `;
 
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
 
-  const page = await browser.newPage();
+    const page = await browser.newPage();
 
-  await page.setContent(html, { waitUntil: ['domcontentloaded', 'networkidle0'] });
-  await page.emulateMedia('screen');
+    await page.setContent(html, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+    await page.emulateMedia('screen');
 
-  const pdf = await page.pdf({
-    path: 'CV.pdf',
-    format: 'A4',
-    scale: 0.75,
-    printBackground: true,
-  });
+    const pdf = await page.pdf({
+      path: `${data.firstName}_${data.lastName}_CV.pdf`,
+      format: 'A4',
+      scale: 0.75,
+      printBackground: true,
+    });
 
-  await browser.close();
-  return pdf;
+    await browser.close();
+    return pdf;
+  } catch (error) {
+    res.status(500);
+    console.log(error);
+  }
 });
 
 app.listen(SERVER_PORT, function () {
