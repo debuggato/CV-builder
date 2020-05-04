4f89aefbe0f2b2378c6d173981bda3fd5c572e50
Ivan Lori <ivan.lori@protonmail.com>
2020-05-04 21:46:23 +0200

    removes console.log

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,11 +1,9 @@
 export const getFormattedDateWithoutDays = (date: string): string => {
-  console.log('Wed Apr 01 2020 15:37:32 GMT+0200 (Central European Summer Time)')
   const formattedDate = new Date(date)
 
   const year = formattedDate.getFullYear();
 
   let month = (1 + formattedDate.getMonth()).toString();
-
 
   month = month.length > 1 ? month : `0${month}`;
 
