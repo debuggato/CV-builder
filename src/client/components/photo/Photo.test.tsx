80f2431d34331ecab958727304a8d5cae0db739e
Ivan Lori <ivan.lori@protonmail.com>
2020-05-06 08:52:21 +0200

    adds photo component test

---
+++ src/client/components/photo/Photo.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import PhotoView from './Photo.view';
+
+describe('', () => {
+  it('expects to render photo component WITH photo url set', () => {
+
+    const onUpload = () => { }
+
+    const wrapper = renderer
+      .create(
+        <PhotoView onUpload={onUpload} imgUrl="/assets/photo_profile.jpg" />
+      )
+      .toJSON();
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
