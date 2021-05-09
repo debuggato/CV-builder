3478c54ce1ddd6163d8fd33ac4bf7fcd9a3aea2e
Ivan Lori <ivan.lori@protonmail.com>
2021-05-09 14:20:58 +0200

    Refactor: removes styled component and rewrites function

---
+++ src/client/components/SectionTitle.tsx
@@ -0,0 +1,13 @@
+import React, { FC, ReactElement } from 'react';
+
+type Props = {
+	children: string;
+}
+
+const SectionTitle: FC<Props> = ({
+	children
+}: Props): ReactElement => (
+	<h4 className="pb-2 mb-5 border">{children}</h4>
+)
+
+export default SectionTitle;
