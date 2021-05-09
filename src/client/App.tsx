23c36579d575838e80620bbd8d86ff2240b43fd3
Ivan Lori <ivan.lori@protonmail.com>
2021-05-09 21:18:46 +0200

    Refactor: rewrites resume component

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,6 +1,6 @@
 import React, { FC, ReactElement } from 'react';
 import { withTranslation } from 'react-i18next';
-import Resume from './views/resume/Resume.container';
+import Resume from './views/resume/Resume';
 import Sidebar from './views/sidebar';
 import './styles/global.css';
 
---
+++ src/client/views/resume/Resume.tsx
@@ -0,0 +1,69 @@
+import React, { FC, ReactElement, ReactNode, useState } from 'react';
+import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
+import { Dispatch } from 'redux';
+import { modalDisplayedAction } from '../../store/actions/Root.action';
+import { useTranslation } from 'react-i18next';
+import Modal from '../../components/modal';
+import TemplateMiniature from '../_components/TemplateMiniature';
+import ResumeActions from './ResumeActions';
+import DaVinci from '../../templates/davinci';
+import './Resume.css';
+
+const templates = [
+	'DaVinci',
+	'Michelangelo',
+	'Donatello',
+	'Raffaello',
+	'Caravaggio',
+];
+
+const Resume: FC = (): ReactElement => {
+	const { t } = useTranslation();
+	const {
+		modalDisplayed
+	} = useSelector((state: RootStateOrAny) => state.root);
+	const closeModal = useDispatch<Dispatch>();
+	const [templateSelected, setTemplateSelected] = useState<string | null>(null);
+
+	const getTemplateMiniature = (): ReactNode => (
+		templates.map((template: any, index: number) => (
+			<TemplateMiniature
+				key={index}
+				id={index}
+				onClick={e => {
+					setTemplateSelected(e.currentTarget.id);
+					closeModal(modalDisplayedAction(false));
+				}}>
+				{template[1]}
+			</TemplateMiniature>
+		))
+	)
+
+	const getTemplateTitle = () => {
+		return templateSelected && templates[parseInt(templateSelected)];
+	}
+
+	return (
+		<div className="resume-wrapper">
+			<h4><strong>{getTemplateTitle()}</strong></h4>
+			{templateSelected === '0' && <div className="page"><DaVinci /></div>}
+
+			<ResumeActions
+				templateSelected="0"
+			/>
+
+			{modalDisplayed &&
+				<Modal
+					title={t('choose.template')}
+					header
+				>
+					<div className="flex w-full flex-wrap justify-center overflow-y-scroll">
+						{getTemplateMiniature()}
+					</div>
+				</Modal>
+			}
+		</div>
+	)
+}
+
+export default Resume;
