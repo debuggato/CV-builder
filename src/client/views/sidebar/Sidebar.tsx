a5a9f628ac285a84a4a1f3f6a3126fc233b98af6
Ivan Lori <ivan.lori@protonmail.com>
2021-05-09 21:06:00 +0200

    Refactor: rewrites sidebar component

--- src/client/views/sidebar/Sidebar.tsx
+++ src/client/views/sidebar/Sidebar.tsx
@@ -1,95 +1,77 @@
-import React, { Component, ReactNode } from 'react';
+import React, { ReactElement, FC, useState } from 'react';
 import { CSSProperties } from 'styled-components';
 import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import { withTranslation, WithTranslation } from 'react-i18next';
-import IconView from '../../components/Icon.view';
+import { useTranslation } from 'react-i18next';
+import Icon from '../../components/Icon';
 import Button from '../../components/button';
-import { Container, ActionBar, SectionWrapper } from './Sidebar.style';
 import Details from '../../views/details/Details.container';
-import Summary from '../../views/summary/Summary.container';
+import Summary from '../summary/Summary';
 import Education from '../../views/education/Education.container';
 import Employment from '../../views/employment/Employment.container';
 import Skills from '../../views/skills/Skills.container';
 import Links from '../../views/links/Links.container';
-import Header from '../../views/header/Header.container';
-import AddSection from '../../views/add_section/AddSection.container';
+import Header from '../header/Header';
+import AddSection from '../add_section/AddSection';
+import './Sidebar.css';
 
-type State = {
-	currentStep: number;
-	sectionToShow: number;
-}
+const arrowNext: CSSProperties = {
+	marginLeft: '10px',
+};
 
-class Sidebar extends Component<WithTranslation, State> {
-	state = {
-		currentStep: 1,
-		sectionToShow: 7,
-	};
+const arrowPrev: CSSProperties = {
+	marginRight: '10px',
+};
 
-	onNext = (): void => {
-		const { sectionToShow, currentStep } = this.state;
+const Sidebar: FC = (): ReactElement => {
+	const [currentStep, setCurrentStep] = useState<number>(1);
+	const sectionToShow = 7;
+	const { t } = useTranslation();
+
+	const onNext = (): void => {
 		let step: number = currentStep;
 
 		step = currentStep === sectionToShow ? sectionToShow : currentStep + 1;
 
-		this.setState({
-			currentStep: step,
-		});
+		setCurrentStep(step)
 	};
 
-	onPrev = (): void => {
-		const { currentStep } = this.state;
-
+	const onPrev = (): void => {
 		let step: number = currentStep;
 
 		step = currentStep <= 1 ? step = 1 : step - 1;
 
-		this.setState({
-			currentStep: step,
-		});
+		setCurrentStep(step);
 	};
 
-	public render(): ReactNode {
-		const { currentStep, sectionToShow } = this.state;
-		const { t } = this.props;
+	return (
+		<div className="container">
+			<Header />
+			<div className="wrapper">
+				<Details currentStep={currentStep} />
+				<Summary currentStep={currentStep} />
+				<Employment currentStep={currentStep} />
+				<Education currentStep={currentStep} />
+				<Skills currentStep={currentStep} />
+				<Links currentStep={currentStep} />
+				<AddSection currentStep={currentStep} />
+			</div>
+			<div className="action-bar">
+				{currentStep !== 1 &&
+					<Button onClick={() => onPrev} style={Button.SECONDARY}>
+						<Icon icon={faArrowLeft} style={arrowPrev} />
+						{t('prev')}
+					</Button>
+				}
 
-		const arrowNext: CSSProperties = {
-			marginLeft: '10px',
-		};
-
-		const arrowPrev: CSSProperties = {
-			marginRight: '10px',
-		};
-
-		return (
-			<Container>
-				<Header />
-				<SectionWrapper>
-					<Details currentStep={currentStep} />
-					<Summary currentStep={currentStep} />
-					<Employment currentStep={currentStep} />
-					<Education currentStep={currentStep} />
-					<Skills currentStep={currentStep} />
-					<Links currentStep={currentStep} />
-					<AddSection currentStep={currentStep} />
-				</SectionWrapper>
-				<ActionBar>
-					{currentStep !== 1 &&
-						<Button onClick={this.onPrev} secondary>
-							<IconView icon={faArrowLeft} style={arrowPrev} />
-							{t('prev')}
-						</Button>
-					}
-
-					{currentStep !== sectionToShow &&
-						<Button onClick={this.onNext} primary>
-							{t('next')}
-							<IconView icon={faArrowRight} style={arrowNext} />
-						</Button>
-					}
-				</ActionBar>
-			</Container>
-		);
-	}
+				{currentStep !== sectionToShow &&
+					<Button onClick={() => onNext} style={Button.PRIMARY}>
+						{t('next')}
+						<Icon icon={faArrowRight} style={arrowNext} />
+					</Button>
+				}
+			</div>
+		</div>
+	);
 }
 
-export default withTranslation()(Sidebar);
+export default Sidebar;
