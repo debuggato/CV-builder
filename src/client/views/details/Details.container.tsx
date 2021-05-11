abc971ad802654f64158fd21a4def3469b29f309
Ivan Lori <ivan.lori@protonmail.com>
2021-05-11 21:51:05 +0200

    Refactor: rewrites details component

--- src/client/views/details/Details.container.tsx
+++ src/client/views/details/Details.container.tsx
@@ -1,58 +1,40 @@
-import React, { Component, ReactNode } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
-import { withTranslation, WithTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import Button from '../../components/button';
-import Title from '../../components/Title.view';
-import IconView from '../../components/Icon.view';
+import SectionTitle from '../../components/SectionTitle';
+import Icon from '../../components/Icon';
 import AdditionalDetails from './AdditionalDetails.view';
-import MainDetails from './MainDetails.view';
+import MainDetails from './MainDetails';
 import { Container, ButtonWrapper } from './Details.style';
 
-interface State {
-	isOpen: boolean;
-}
-
-interface OwnProps extends WithTranslation {
+type Props = {
 	currentStep: number;
 }
 
-class Details extends Component<OwnProps, State> {
-	state = {
-		isOpen: false,
-	};
+const Details: FC<Props> = ({ currentStep }: Props): ReactElement => {
+	const [open, setOpen] = useState(false);
+	const { t } = useTranslation();
 
-	showAdditionalDetails = (): void => {
-		this.setState({
-			isOpen: !this.state.isOpen,
-		});
-	};
+	if (currentStep !== 1) return <></>;
 
-	public render(): ReactNode {
-		const { currentStep, t } = this.props;
-
-		if (currentStep !== 1) return null;
-
-		const { isOpen } = this.state;
-
-		return (
-			<Container>
-				<Title>{t('personal.details')}</Title>
-				<MainDetails />
-				<ButtonWrapper onClick={this.showAdditionalDetails}>
-					<Button
-						linkStyle
-						primary
-					>
-						{t('edit.additional.details')}
-					</Button>
-					<IconView icon={isOpen ? faChevronUp : faChevronDown} />
-				</ButtonWrapper>
-				{isOpen &&
-					<AdditionalDetails />
-				}
-			</Container>
-		);
-	}
+	return (
+		<Container>
+			<SectionTitle>{t('personal.details')}</SectionTitle>
+			<MainDetails />
+			<ButtonWrapper onClick={() => setOpen(!open)}>
+				<Button
+					style={Button.PRIMARY}
+				>
+					{t('edit.additional.details')}
+				</Button>
+				<Icon icon={open ? faChevronUp : faChevronDown} />
+			</ButtonWrapper>
+			{open &&
+				<AdditionalDetails />
+			}
+		</Container>
+	);
 }
 
-export default withTranslation()(Details);
+export default Details;
