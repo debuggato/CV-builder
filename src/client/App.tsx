59de5cdb6a97f536f3911f8dccc242448835b823
Ivan Lori <ivan.lori@protonmail.com>
2021-12-08 17:51:40 +0100

    fix: style with tailwind configuration

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,7 @@
 import React, { FC, ReactElement } from 'react';
 import Resume from './views/resume/Resume';
 import Sidebar from './views/sidebar';
-import './styles/global.css';
+import "tailwindcss/dist/tailwind.css"
 
 const App: FC = (): ReactElement => (
 	<div className="flex h-screen">
