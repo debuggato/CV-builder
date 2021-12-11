0c3783ae4b29a9e3982568df4f00d8fc33fdceda
Ivan Lori <ivan.lori@protonmail.com>
2021-12-11 18:04:58 +0100

    adds /api base route

--- src/server/index.js
+++ src/server/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(cors(corsOptions));
 
-app.use('/render', renderController);
+app.use('/api/render', renderController);
 
 app.listen(process.env.PORT || 5000);
 
