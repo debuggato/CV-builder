4282d2ad8e2859c9523d8ddfe8d9f9db38d4a089
Ivan Lori <ivan.lori@protonmail.com>
2021-12-11 18:14:31 +0100

    deploy 4

--- src/config.ts
+++ src/config.ts
@@ -4,18 +4,14 @@ const CLIENT_PORT = 8080;
 const SERVER_PORT = 5000;
 const SERVER_URL = isDev ? `http://localhost:${SERVER_PORT}` : 'https://cv-builder-three.vercel.app';
 const CLIENT_URL = isDev ? `http://localhost:${CLIENT_PORT}` : 'https://cv-builder-three.vercel.app';
-const RENDER_ENDPOINT = '/render';
-const UPLOAD_ENDPOINT = '/upload';
+const RENDER_ENDPOINT = '/api/render';
 const RENDER_URL = `${SERVER_URL + RENDER_ENDPOINT}`;
-const UPLOAD_URL = `${SERVER_URL + UPLOAD_ENDPOINT}`;
 
 interface ConfigModel {
 	server_url: string;
 	client_url: string;
 	render_url: string;
-	upload_url: string;
 	render_endpoint: string;
-	upload_endpoint: string;
 	client_port: number;
 }
 
@@ -23,9 +19,7 @@ const config: ConfigModel = {
 	server_url: SERVER_URL,
 	client_url: CLIENT_URL,
 	render_url: RENDER_URL,
-	upload_url: UPLOAD_URL,
 	render_endpoint: RENDER_ENDPOINT,
-	upload_endpoint: UPLOAD_ENDPOINT,
 	client_port: CLIENT_PORT
 };
 
